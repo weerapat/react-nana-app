d29053dd9332d916977ad523d689f30825c036c6
Weerapat <weerapatsu@gmail.com>
2020-02-04 16:39:19 +0700

    Add basic calendar component to test visual design

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import { Button } from 'antd';
 import './App.css';
 import AppLayout from './Layout';
 
--- src/Layout.js
+++ src/Layout.js
@@ -1,8 +1,68 @@
 import React from 'react';
-import {Layout, Menu, Breadcrumb, Icon} from 'antd';
+import {Layout, Menu, Breadcrumb, Icon, Calendar, Badge} from 'antd';
 
 const {Header, Content, Footer, Sider} = Layout;
 const {SubMenu} = Menu;
+
+function getListData(value) {
+  let listData;
+  switch (value.date()) {
+    case 8:
+      listData = [
+        { type: 'warning', content: 'This is warning event.' },
+        { type: 'success', content: 'This is usual event.' },
+      ];
+      break;
+    case 10:
+      listData = [
+        { type: 'warning', content: 'This is warning event.' },
+        { type: 'success', content: 'This is usual event.' },
+        { type: 'error', content: 'This is error event.' },
+      ];
+      break;
+    case 15:
+      listData = [
+        { type: 'warning', content: 'This is warning event' },
+        { type: 'success', content: 'This is very long usual event。。....' },
+        { type: 'error', content: 'This is error event 1.' },
+        { type: 'error', content: 'This is error event 2.' },
+        { type: 'error', content: 'This is error event 3.' },
+        { type: 'error', content: 'This is error event 4.' },
+      ];
+      break;
+    default:
+  }
+  return listData || [];
+}
+
+function dateCellRender(value) {
+  const listData = getListData(value);
+  return (
+    <ul className="events">
+      {listData.map(item => (
+        <li key={item.content}>
+          <Badge status={item.type} text={item.content} />
+        </li>
+      ))}
+    </ul>
+  );
+}
+
+function getMonthData(value) {
+  if (value.month() === 8) {
+    return 1394;
+  }
+}
+
+function monthCellRender(value) {
+  const num = getMonthData(value);
+  return num ? (
+    <div className="notes-month">
+      <section>{num}</section>
+      <span>Backlog number</span>
+    </div>
+  ) : null;
+}
 
 class AppLayout extends React.Component {
   state = {
@@ -66,9 +126,12 @@ class AppLayout extends React.Component {
               <Breadcrumb.Item>User</Breadcrumb.Item>
               <Breadcrumb.Item>Bill</Breadcrumb.Item>
             </Breadcrumb>
-            <div style={{padding: 24, background: '#fff', minHeight: 360}}>Bill is a cat.</div>
+            <div style={{padding: 24, background: '#fff', minHeight: 360}}>
+              Bill is a cat.
+              <Calendar dateCellRender={dateCellRender} monthCellRender={monthCellRender} />
+            </div>
           </Content>
-          <Footer style={{textAlign: 'center'}}>Ant Design ©2018 Created by Ant UED</Footer>
+          <Footer style={{textAlign: 'center'}}>Top Design ©2020</Footer>
         </Layout>
       </Layout>
     );
